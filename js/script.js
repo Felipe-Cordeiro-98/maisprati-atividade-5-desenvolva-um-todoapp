73071b4a39eb10127dff146a38efe035411e62fd
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2024-11-17 15:06:30 -0300

    creat functionality add task, count tasks

--- js/script.js
+++ js/script.js
@@ -38,3 +38,66 @@ if (theme === "1") {
 } else if (theme === "2") {
     backgroundChange(2);
 }
+
+/* add tasks */
+
+const button = document.querySelector(".button-submit");
+
+button.addEventListener("click", () => {
+    let task = document.querySelector("#task-input");
+
+    let containerTask = document.createElement("div");
+    containerTask.classList.add("task");
+
+    let checkbox = document.createElement("div");
+    checkbox.classList.add("checkbox");
+
+    let inputTask = document.createElement("input");
+    inputTask.classList.add("input-task");
+    inputTask.setAttribute("type", "text");
+    inputTask.setAttribute("value", task.value);
+    inputTask.setAttribute("readonly", true);
+
+    inputTask.onclick = () => {
+        inputTask.removeAttribute("readonly");
+        inputTask.focus();
+    };
+
+    let buttonRemove = document.createElement("button");
+    buttonRemove.classList.add("btn", "btn-danger", "btn-sm");
+    buttonRemove.textContent = "Remover";
+
+    buttonRemove.addEventListener("click", () => {
+        containerTask.remove();
+        updateTaskCount();
+    });
+
+    containerTask.appendChild(checkbox);
+    containerTask.appendChild(inputTask);
+    containerTask.appendChild(buttonRemove);
+
+    let container = document.querySelector(".container-tasks");
+
+    container.prepend(containerTask);
+    task.value = "";
+    updateTaskCount();
+});
+
+function updateTaskCount() {
+    const infoContainer = document.querySelector(".info-container");
+    let totalTasks = document.querySelectorAll(".task").length;
+
+    let total = document.querySelector("#total-tasks");
+
+    if (!total) {
+        total = document.createElement("span");
+        total.id = "total-tasks";
+        infoContainer.prepend(total);
+    }
+    if (totalTasks < 1) {
+        total.remove();
+    } else {
+        let text = totalTasks < 2 ? "tarefa" : "tarefas";
+        total.innerText = `${totalTasks} ${text}`;
+    }
+}
